e4751138d00885b1e544524a6a98eaf757806653
pramod kamble <kamblepramod806@gmail.com>
2024-04-16 21:47:39 +0530

    password hashing added

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,8 +1,27 @@
 import { Request, Response, NextFunction } from "express"
+import createHttpError from "http-errors";
+import bcrypt from 'bcrypt'
+import User from './userModel'
 
 export const createUser = async(req:Request, res:Response, next:NextFunction) => {
-    
-    res.json({
-        message: req.body['name']
+    const {name, email, password} = req.body;
+    if(!name || !email || !password) {
+        const error = createHttpError(400, 'All fields are required')
+        return next(error)
+    }
+    const user = await User.findOne({email})
+    if(user) {
+        const error = createHttpError(400, 'Email already in use.')
+        return next(error)
+    }
+
+    // password hashing before save
+    const hashedPassword = bcrypt.hash(password, 8)
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
+
+    
 }
