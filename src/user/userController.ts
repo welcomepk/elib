1ba174a4f905c9dd1734fad9ebbb7e361f0dfbad
pramod kamble <kamblepramod806@gmail.com>
2024-04-16 21:34:11 +0530

    add Usermodel

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 
 export const createUser = async(req:Request, res:Response, next:NextFunction) => {
+    
     res.json({
         message: req.body['name']
     })
