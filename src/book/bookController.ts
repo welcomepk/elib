efeb834e3d323403bc0d908ebdbf648a6e3ddb75
pramod kamble <kamblepramod806@gmail.com>
2024-04-18 07:36:16 +0530

    BookModel add

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express"
+import { Book } from "./bookTypes"
+import bookModel from "./bookModel"
 
 export const createBook = async(req:Request, res:Response, next:NextFunction) =>{
     res.send('book created')
