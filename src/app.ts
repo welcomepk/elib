780007d055fea10d31fbfdda98ddbd26f559d56a
pramod kamble <kamblepramod806@gmail.com>
2024-04-21 11:33:34 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import userRouter from './user/userRouter'
 import bookRouter from './book/bookRouter'
@@ -6,7 +7,9 @@ import bookRouter from './book/bookRouter'
 const app = express()
 
 app.use(express.json())
-
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
 
 
 
